ead058b8888eb9635a6e3645ce9cc63146f7ba69
Westraze <114066515+EsKkadschalis@users.noreply.github.com>
2023-03-16 13:38:47 +0200

    Delete revision.py

--- revision.py
+++
@@ -1,8 +0,0 @@
-def SumTwoNumbers(a, b):
-    return a+b
-
-def JoinStings(text1, text2, delimiter):
-    return f"{text1}{delimiter}{text2}"
-
-result = JoinStings('Coputer', 'Coding', '.')
-print(result)
